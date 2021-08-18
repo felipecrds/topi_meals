56169b1a2e0fd8d08da7a6b8ac8cf34e74903d10
Felipe Cardoso <71651685+felpsc@users.noreply.github.com>
2021-08-18 10:43:08 -0300

    Update UserReducer.js

--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -4,13 +4,12 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
+    // eslint-disable-next-line default-case
     switch (action.type) {
         case 'SET_TOKEN':
             return { ...state, token: action.payload.token };
-            break;
         case 'SET_NAME':
             return { ...state, name: action.payload.name };
-            break;
     }
 
     return state;
